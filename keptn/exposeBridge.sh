909794da0c17333690ad6b0f679b39515cc2c379
agrimmer <andreas.grimmer@dynatrace.com>
2020-01-31 17:25:28 +0100

    Refine setup

--- keptn/exposeBridge.sh
+++ keptn/exposeBridge.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Update Bridge to Early Access Version"
+kubectl -n keptn set image deployment/bridge bridge=keptn/bridge2:0.6.1.EAP.20200131.1010 --record
+kubectl -n keptn-datastore set image deployment/mongodb-datastore mongodb-datastore=keptn/mongodb-datastore:0.6.1.EAP.20200131.1010 --record
 
 echo ""
 echo "============================================================="
